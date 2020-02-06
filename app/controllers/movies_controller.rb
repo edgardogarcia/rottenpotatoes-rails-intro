12226d45d54bbd07bfa2f1470d4a949645be998a
EC2 Default User <ec2-user@ip-172-31-41-21.us-east-2.compute.internal>
2020-02-06 05:01:41 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,8 +16,6 @@ class MoviesController < ApplicationController
     
     if(sort_type)
       @movies = Movie.order(sort_type)
-    elsif(!sort_type)
-      @movies = Movie.where(rating: params[:ratings].keys)
     else
       @movies = Movie.all
     end
