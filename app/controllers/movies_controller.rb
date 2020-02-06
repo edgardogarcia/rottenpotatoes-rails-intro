ce56e06e19f10c620c4519f2a5a055102162a12a
EC2 Default User <ec2-user@ip-172-31-41-21.us-east-2.compute.internal>
2020-02-06 04:56:26 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,10 +12,12 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = ['G','PG','PG-13','R']
-    
     sort_type = params[:inorder]
+    
     if(sort_type)
       @movies = Movie.order(sort_type)
+    elsif(!sort_type && (@all_ratings != params[:ratings].keys))
+      @movies = Movie.where(params[:ratings].keys)
     else
       @movies = Movie.all
     end
