113585fa923248c18387d4a2fb7333a65c6f8ad4
EC2 Default User <ec2-user@ip-172-31-41-21.us-east-2.compute.internal>
2020-02-07 21:41:18 +0000

    testing part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,19 +26,19 @@ class MoviesController < ApplicationController
     end
     
     
-    if(session[:ratings] and ratings_list)
-      if(session[:inorder])
-        @movies = Movie.where(session[:ratings].keys).order(session[:inorder])
-      else
-        @movies = Movie.where(session[:ratings].keys)
-      end
-    elsif(session[:rating] and !ratings_list and session[:inorder])
-      redirect_to movies_path(ratings: session[:ratings], inorder: session[:inorder])
-    else
-       @movies = Movie.all
-    end
+ #   if(session[:ratings] and ratings_list)
+  #    if(session[:inorder])
+   #     @movies = Movie.where(session[:ratings].keys).order(session[:inorder])
+    #  else
+     #   @movies = Movie.where(session[:ratings].keys)
+#      end
+ #   elsif(session[:rating] and !ratings_list and session[:inorder])
+ #     redirect_to movies_path(ratings: session[:ratings], inorder: session[:inorder])
+  #  else
+    #   @movies = Movie.all
+   # end
        
-    return @movies
+    return @movies.all
   end
 
   def new
